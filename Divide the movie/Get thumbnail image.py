f9b3d5bff59cdf8d28d5450db049850402efcf2e
checkJ <j45shizuoka.dd@gmail.com>
2018-06-21 20:43:24 +0900

    サムネ獲得プログラムの修正 全ては治ってない

--- Divide the movie/Get thumbnail image.py
+++ Divide the movie/Get thumbnail image.py
@@ -3,14 +3,17 @@ import os
 import numpy as np
 import time
 
-input_path = "D:\jifuku\M2\プログラミングpython\image proccesing\Binary\sample"
+input_path = "D:\jifuku\M2\プログラミングpython\Image Processing\Divide the movie\sample"
 file_number = 2
 
 
 for i in range(int(file_number)):
+    cd = os.getcwd()
+    print(cd)
     input_file = str(i) + ".mp4"
     print("動画の名前 %s" % input_file)
     # 動画の読み込み。ここで指定したインスタンスが以降の動画操作系のコマンドに継承されていく
+    os.chdir(input_path)
     cap = cv2.VideoCapture(input_file)
     # スクリーンキャプチャを保存するdirectory設定
     dir_name = "frame"
@@ -24,15 +27,18 @@ for i in range(int(file_number)):
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = cap.get(5)
 
-# 一つ目の変数にはフレームが読み込まれているかの確認。
-# 二つ目の変数には画像の配列が格納される
-ret, frame = cap.read()
+    # 一つ目の変数にはフレームが読み込まれているかの確認。
+    # 二つ目の変数には画像の配列が格納される
+    ret, frame = cap.read()
+    print(frame)
+    # 読み込んだ動画の情報および一枚目を出力
+    print("フレーム数")
+    print(frame_count)
+    print("フレームレート")
+    print(fps)
 
-# 読み込んだ動画の情報および一枚目を出力
-print("フレーム数")
-print(frame_count)
-print("フレームレート")
-print(fps)
-print("--------------------------------------------------")
-save_name = str(save_path) + "/" + str(i) + ".png"
-cv2.imwrite(save_name, frame)
+    save_name = str(save_path) + "\ " + str(i) + ".png"
+    print(save_name)
+
+    cv2.imwrite(save_name, frame)
+    print("--------------------------------------------------")
