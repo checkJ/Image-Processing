62c8115ec1d9fdf3b677f1204745d9ce967e715c
checkJ <j45shizuoka.dd@gmail.com>
2018-06-29 14:53:27 +0900

    入力だいるのdirectory指定について修正

--- ROI/Trim.py
+++ ROI/Trim.py
@@ -5,41 +5,35 @@ import os
 
 #ROIの起点
 ROI_s =[710, 620]
+
 #ROIの終点
 ROI_e = [810, 740]
 
-file_path = "F:\\Deep Learning\\0625\\images"
-os.chdir(file_path)
+#トリミングしたい画像が入っているdirectorypath
+input_path = "F:\\Deep Learning\\0625\\images"
 
+
+os.chdir(input_path)
 cd = os.getcwd()
-print(cd)
+
+#保存するdirectory name
 dir_name = "templates"
 if not os.path.exists(dir_name):
     os.mkdir(dir_name)
 
+#directory内部のい画像枚数を調べる
+number_images = []
+number_images =os.listdir(input_path)
 
 
-for i in range (50):
+for i in range (len(number_images)):
     img_name = str(i)+".bmp"
     print(img_name)
     input_img = cv2.imread(img_name)
     trim = input_img[620:740, 710:810]
-    save_name1 = "trim%2d.bmp" %i
+    save_name1 = "trim%d.bmp" %i
     cv2.imwrite(dir_name+"\\"+save_name1,trim)
 
 os.chdir(cd)
 
-dir_name = "Blur_templates"
-if not os.path.exists(dir_name):
-    os.mkdir(dir_name)
-
-
-for i in range (50):
-    input_img = cv2.imread(str(i) + ".bmp")
-    blur = cv2.blur(input_img, (10, 10))
-
-    trim_blur = blur[620:740, 710:810]
-    save_name2 = "Blur_trim%2d.bmp" % i
-
-    cv2.imwrite(dir_name + "\\"+save_name2, trim_blur)
 
